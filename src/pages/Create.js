a4f60c7fe7c35648eca527b144553e7b27a3b5ef
Allanetizen <allankosgei747@gmail.com>
2022-02-20 16:20:40 +0300

    noWrap text truncation and color in Typography component

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import Typography from '@mui/material/Typography';
-
-
+import React from "react";
+import Typography from "@mui/material/Typography";
 
 export default function Create() {
   return (
     <div>
-      
-     <Typography variant="h1" component="h2">
-       Create a new Note</Typography>  
+      {/* <Typography 
+     variant="h1"
+      component="h2"
+      color="primary"
+      align="center">
+       Create a new Note
+       </Typography>   */}
+      <Typography noWrap  
+                  color="secondary" 
+                  >
+        Lorem ipsum dolor sit amet, consectetur adip, Lorem ipsum dolor sit
+        amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip
+        Lorem ipsum olor sit amet, consectetur adipLorem ipsum dolor sit amet,
+        consectetur adipLorem ipsum dolor sit amet, consectetur adipvLorem ipsum
+        dolor sit amet, consectetur adipvLorem ipsum dolor sit amet, consectetur
+        adip
+      </Typography>
     </div>
-  )
+  );
 }
