d3d236a6e98b89029f6388c57f7562e961efe6a6
Allanetizen <allankosgei747@gmail.com>
2022-02-20 13:42:23 +0300

    updated MUI

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Typography from '@mui/material/Typography';
+
+
 
 export default function Create() {
   return (
     <div>
-      Create page
+      
+     <Typography variant="h1" component="h2">
+       Create a new Note</Typography>  
     </div>
   )
 }
